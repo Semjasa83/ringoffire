0a22428a27b8640a2c77318d28b9edddb2751de8
MarceldZ <94352026+Semjasa83@users.noreply.github.com>
2022-08-05 01:52:03 +0200

    player inserted

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,18 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { Game } from 'src/models/game';
 
-
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss']
+  styleUrls: ['./game.component.scss'],
 })
 export class GameComponent implements OnInit {
-
   pickCardAnimation = false;
+  currentCard: string = '';
   game: Game;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     this.newGame();
@@ -20,10 +19,19 @@ export class GameComponent implements OnInit {
 
   newGame() {
     this.game = new Game();
-    console.log(this.game);
   }
 
   takeCard() {
-    this.pickCardAnimation = true
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop(); //pop last value from Array and splice from Array
+      console.log(this.currentCard);
+      this.pickCardAnimation = true;
+      console.log('Game is', this.game);            //__CONSOLE
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.pickCardAnimation = false;
+        console.log('New Card: ' + this.currentCard); //__CONSOLE
+      }, 1300);
+    }
   }
 }
