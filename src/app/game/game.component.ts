70ac880ba79b44f4e1b5874da19d6417e8568ceb
MarceldZ <94352026+Semjasa83@users.noreply.github.com>
2022-08-04 15:00:55 +0200

    animate first card

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
+  pickCardAnimation = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  takeCard() {
+    this.pickCardAnimation = true
+  }
 }
