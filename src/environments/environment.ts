b7d4aec7c051771c204be580e9542491e8299b6c
MarceldZ <94352026+Semjasa83@users.noreply.github.com>
2022-08-09 00:56:12 +0200

    install firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'ringoffire-firebase',
+    appId: '1:438763677792:web:bc9517fd88159841c2fb05',
+    storageBucket: 'ringoffire-firebase.appspot.com',
+    apiKey: 'AIzaSyBfuRUBZ9QARq8PCzXBYcd_JuDbcnYL280',
+    authDomain: 'ringoffire-firebase.firebaseapp.com',
+    messagingSenderId: '438763677792',
+    measurementId: 'G-YP38NM761H',
+  },
   production: false
 };
 
