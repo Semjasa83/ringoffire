f426f1ffd2709e4fa6c43d120183fed4c3c07260
MarceldZ <94352026+Semjasa83@users.noreply.github.com>
2022-08-09 01:57:39 +0200

    push game to JSON firebase

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Game } from 'src/models/game';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { doc, setDoc } from "firebase/firestore";
 
 @Component({
   selector: 'app-game',
@@ -12,15 +15,26 @@ export class GameComponent implements OnInit {
   pickCardAnimation = false;
   currentCard: string = '';
   game: Game;
+  games: Observable<Game[]>;
 
-  constructor(public dialog: MatDialog) {}
+  constructor(private firestore: Firestore, public dialog: MatDialog) {}
+
+  coll = collection(this.firestore, 'games');
+  games$ = collectionData(this.coll);
 
   ngOnInit(): void {
     this.newGame();
+    this
+      .games$
+      .subscribe((game) => {
+        console.log('gameupdate', game);
+    });
+    ;
   }
 
   newGame() {
     this.game = new Game();
+    setDoc(doc(this.coll), this.game.toJSON());
   }
 
   takeCard() {
--- src/models/game.ts
+++ src/models/game.ts
@@ -15,26 +15,37 @@ export class Game {
         shuffle(this.stack);
 
     }
+
+    public toJSON(){
+      return {
+        players: this.players,
+        stack: this.stack,
+        playedCards: this.playedCards,
+        currentPlayer: this.currentPlayer
+      }
+    }
 }
+
+
 
 function shuffle(array) {
     let currentIndex = array.length,  randomIndex;
-  
+
     // While there remain elements to shuffle.
     while (currentIndex != 0) {
-  
+
       // Pick a remaining element.
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex--;
-  
+
       // And swap it with the current element.
       [array[currentIndex], array[randomIndex]] = [
         array[randomIndex], array[currentIndex]];
     }
-  
+
     return array;
   }
-  
+
   // Used like so
   var arr = [2, 11, 37, 42];
   shuffle(arr);
